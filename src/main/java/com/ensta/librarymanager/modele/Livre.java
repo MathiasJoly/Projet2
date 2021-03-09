06570f86fc099b8829c610851a623b8a54f9c048
Joly Mathias <mathias.joly@ensta-paris.fr>
2021-03-09 11:21:12 +0100

    Exercice 2

--- src/main/java/com/ensta/librarymanager/modele/Livre.java
+++ src/main/java/com/ensta/librarymanager/modele/Livre.java
@@ -1,1 +1,64 @@
-package com.ensta.librarymanager.modele
+package com.ensta.librarymanager.modele;
+
+public class Livre 
+{
+	int id;
+	String titre;
+	String auteur;
+	String isbn;
+
+	public int getId() 
+	{
+		return id;
+	}
+
+	public void setId(int unId) 
+	{
+		this.id = unId;
+	}
+
+	public String getTitre() 
+	{
+		return titre;
+	}
+
+	public void setTitre(String unTitre) 
+	{
+		this.titre = unTitre;
+	}
+
+	public String getAuteur() 
+	{
+		return auteur;
+	}
+
+	public void setAuteur(String unAuteur) 
+	{
+		this.auteur = unAuteur;
+	}
+
+	public String getIsbn() 
+	{
+		return isbn;
+	}
+
+	public void setIsbn(String unIsbn) 
+	{
+		this.isbn = unIsbn;
+	}
+
+    	public Livre() 
+	{}
+
+	public Livre(int unId, String unTitre, String unAuteur, String unIsbn) 
+	{
+		this.id = unId;
+		this.titre = unTitre;
+		this.auteur = unAuteur;
+		this.isbn = unIsbn;
+	}
+
+	public String toString() 
+	{ return "Livre [id=" + id + ", titre=" + titre + ", auteur=" + auteur + ", isbn=" + isbn + "]";}
+
+}
---
+++ src/main/java/com/ensta/librarymanager/test/ModeleTest.java
@@ -0,0 +1,20 @@
+package com.ensta.librarymanager.test;
+
+import com.ensta.librarymanager.modele.*;
+
+import java.lang.Object ;
+import java.time.LocalDate ; 
+
+public class ModeleTest 
+{
+	public static void main( String[] args )
+	{
+		int id = 1;
+		Membre membre = new Membre(id, "Joly", "Mathias", "4 rue Bailly 75003 Paris", "mathias.joly@ensta-paris.fr", "06 93 91 94 95", Abonnement.VIP);
+		Livre livre = new Livre(id, "L'insoutenable légèreté de l'être", "Milan Kundera", "817525766-0");
+		LocalDate dateEmprunt = LocalDate.of(2021,3,9);
+		LocalDate dateRetour = LocalDate.of(2021,3,30);
+		Emprunt emprunt = new Emprunt(id, membre, livre, dateEmprunt, dateRetour);
+		System.out.println(emprunt.toString());
+	}
+}
